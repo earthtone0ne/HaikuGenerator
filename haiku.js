9d0839721ad58958db1c4c6e25f7b307462e0e26
Stephanie Adams <adamsstephaniem@gmail.com>
2016-06-03 14:46:40 -0400

    Haiku command accepts arguments for line len

--- haiku.js
+++ haiku.js
@@ -1,4 +1,14 @@
 var hg = require('./haiku_generator');
-//console.log(hg);
-//hg.setStructure();
-hg.generatePoem([[5],[7],[5]]);
+
+var argNum = process.argv.length;
+var syllArray = [];
+
+if (argNum ==2) {syllArray= [[5],[7],[5]];}
+	
+	else {for (var i=2;i<argNum;i++) {
+			syllArray.push([process.argv[i]]);
+		}
+	}
+
+
+hg.generatePoem(syllArray);
